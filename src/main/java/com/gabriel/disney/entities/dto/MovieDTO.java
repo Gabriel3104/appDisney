fe0b71768790ebe4df3582cf39abeea8a278d3fa
Gabriel Saleha <gabrielsaleha9@gmail.com>
2022-05-30 17:59:02 -0300

    Add MovieDTO

---
+++ src/main/java/com/gabriel/disney/entities/dto/MovieDTO.java
@@ -0,0 +1,13 @@
+package com.gabriel.disney.entities.dto;
+
+import java.time.LocalDate;
+
+public class MovieDTO {
+
+    //private image;
+    private String title;
+    private LocalDate creationDate;
+    private int qualification;
+    private String associateCharacters;
+
+}
